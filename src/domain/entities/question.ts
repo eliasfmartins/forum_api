ae9e78902ea26fef1de7ebee27c69c98f00b7e69
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-30 20:44:02 -0300

    feat: create campo slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,13 @@
 import { randomUUID } from 'node:crypto';
+import { Slug } from './value-objects/slug';
 
 export class Question {
 	constructor(
 		public title: string,
 		public content: string,
 		public authorId: string,
-		public id?: string,
+		public slung: Slug,
+		public id?: string
 	) {
 		this.id = id ?? randomUUID();
 	}
