49f237cb3f859f27a3b8d5acfa36135feaa2e0eb
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-31 11:23:02 -0300

    feat: implementando entity unique

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,15 @@
+import { randomUUID } from 'node:crypto';
+
+export class UniqueEntityID {
+	private value: string;
+	toString() {
+		return this.value;
+	}
+
+	toValue() {
+		return this.value;
+	}
+	constructor(id?: string) {
+		this.value = id ?? randomUUID();
+	}
+}
